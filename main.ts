3eaf76804d817d64e562238d74ffc23bf926912e
T. Hamilton <profhamilton3@gmail.com>
2025-07-15 05:53:46 -0700

    start code.

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+if (ml.isDetected(ml.event.Unknown)) {
+    basic.showIcon(IconNames.Heart)
+} else {
+    basic.showIcon(IconNames.House)
+}
+
+basic.forever(function on_forever() {
+    
 })
